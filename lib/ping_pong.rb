77a266802dcb55df0b327eeb831ae34b75317cb2
Jon Stump <jmstump@gmail.com>
2021-02-22 11:07:18 -0800

    all tests passing
    Co-authored-by: Kody Gordon <gordon.km89@gmail.com>
    Co-authored-by: Jo Miller <joannadawnmiller@gmail.com>
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -5,7 +5,9 @@ def ping_pong(number)
   # number = gets.chomp
   while (x < number)
     x = x + 1
-    if x.%(3).eql?(0)
+    if (x.%(3).eql?(0)) && (x.%(5).eql?(0))
+      number_array.push('ping-pong')
+    elsif x.%(3).eql?(0)
       number_array.push('ping')
     elsif x.%(5).eql?(0)
       number_array.push('pong')
