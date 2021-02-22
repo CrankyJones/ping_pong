ed5edd6f5c0c9ba3aafbe81e271d21562ec114d3
Jon Stump <jmstump@gmail.com>
2021-02-22 10:55:50 -0800

    good fail on pong test
    Co-authored-by: Jo Miller <joannadawnmiller@gmail.com>
    Co-authored-by: Geof Rosenmund <geof.rosenmunds.email@gmail.com>
    Co-authored-by: Kody Gordon <gordon.km89@gmail.com>

--- spec/ping_ping_spec.rb
+++ spec/ping_ping_spec.rb
@@ -8,4 +8,7 @@ describe('#ping_pong') do
   it('replaces all numbers divisble by 3 with "ping"') do
     expect((ping_pong(3))).to(eq([1,2,"ping"]))
   end
+  it('reaplces all number divisible by 5 with "pong"') do
+    expect((ping_pong(5))).to(eq([1,2,"ping",4,"pong"]))
+  end
 end
